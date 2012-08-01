c65a92b8636efd6ca72f1276eeb218ab312bac9c
Kristian Kankainen <kristian.kankainen@kuu.la>
2012-08-02 00:32:03 +0300

    DONE: setup.py TODO: package using sdist

--- setup.py
+++ setup.py
@@ -2,12 +2,19 @@
 
 from distutils.core import setup
 
-setup(name='Lausestaja',
-      version='1.0',
-      description='Jagab teksti lauseteks',
-      author='Kristian Kankainen',
-      author_email='kristian.kankainen@kuu.la',
-      url='http://www.kuu.la/viki/programme',
-      py_modules=['lausestaja'],
-      )
+setup(
+    name='Lausestaja',
+    version='0.1dev',
+    description='Jagab teksti lauseteks',
+    author='Kristian Kankainen',
+    author_email='kristian.kankainen@kuu.la',
+    url='http://www.kuu.la/viki/keeleressursse',
+    packages=['lausestaja',
+              'lausestaja.ortographictext',
+              'lausestaja.ortographicsentence',
+              'lausestaja.ortographicsegmenter'],
+    package_dir={'lausestaja': 'src/lausestaja'},
+    licensce='GNU GPL version 3 or any later version',
+    long_description=open('README.txt').read(),
+    )
 
